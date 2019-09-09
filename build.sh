9d81ccf0479b53d106deea2f9a2271ba056647cf
GaryHjy <782316959@qq.com>
2019-09-09 09:22:53 +0800

    feat: 修改脚本执行代码

--- build.sh
+++ build.sh
@@ -5,3 +5,4 @@ yarn stdver
 git remote add github https://$GITHUB_TOKEN@github.com/GaryHjy/auto-changelog.git > /dev/null 2>&1
 git push github HEAD:master --follow-tags
 
+GREN_GITHUB_TOKEN=$GITHUB_TOKEN yarn release
